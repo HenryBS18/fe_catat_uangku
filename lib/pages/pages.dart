d21c628ab6b8a103ee32d19ba50b5d6ad4a3e1f7
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-05-14 23:48:18 +0800

    Create payment planning page

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 part 'main_page.dart';
 part 'home_page.dart';
 part 'planning_page.dart';
+part 'payment_planning_page.dart';
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,7 @@
 import 'package:fe_catat_uangku/pages/pages.dart';
 import 'package:flutter/material.dart';
 
-Map<String, Widget Function(BuildContext)> routes = {'/main-page': (context) => const MainPage()};
+Map<String, Widget Function(BuildContext)> routes = {
+  '/main-page': (context) => const MainPage(),
+  '/payment-planning-page': (context) => const PaymentPlanningPage(),
+};
