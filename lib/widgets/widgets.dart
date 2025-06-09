9042289ce273130ac9496fd08e962b57a766e61e
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-06-10 00:58:55 +0800

    Remove unused import

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -14,7 +14,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-import 'package:mime/mime.dart';
 
 part 'dots_indicator.dart';
 
