948241b785cf35143eec183993510a9031b727fd
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-06-17 02:41:35 +0800

    Adjust budget model

--- lib/models/budget.dart
+++ lib/models/budget.dart
@@ -1,28 +1,25 @@
 class BudgetModel {
-  final String id;
+  final String? id;
   final String name;
-  final double amount;
-  final double usedAmount;
-  final double remainingAmount;
-  final double percentUsed;
+  final int amount;
+  final List<String> categories;
+  final List<String> walletIds;
+  final String period;
+  final DateTime startDate;
+  final int? usedAmount;
+  final int? remainingAmount;
+  final int? percentUsed;
 
   BudgetModel({
-    required this.id,
+    this.id,
     required this.name,
     required this.amount,
-    required this.usedAmount,
-    required this.remainingAmount,
-    required this.percentUsed,
+    required this.categories,
+    required this.walletIds,
+    required this.period,
+    required this.startDate,
+    this.usedAmount,
+    this.remainingAmount,
+    this.percentUsed,
   });
-
-  factory BudgetModel.fromJson(Map<String, dynamic> json) {
-    return BudgetModel(
-      id: json['_id'],
-      name: json['name'] ?? '-',
-      amount: (json['amount'] as num).toDouble(),
-      usedAmount: (json['usedAmount'] as num).toDouble(),
-      remainingAmount: (json['remainingAmount'] as num).toDouble(),
-      percentUsed: (json['percentUsed'] as num).toDouble(),
-    );
-  }
 }
