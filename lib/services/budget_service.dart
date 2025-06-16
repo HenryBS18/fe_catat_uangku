c9483f95a004d01bdc6300efb9023c3b23396aa7
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-06-17 02:41:48 +0800

    Create budget service

--- lib/services/budget_service.dart
+++ lib/services/budget_service.dart
@@ -1,18 +1,48 @@
-import 'package:dio/dio.dart';
-import 'package:fe_catat_uangku/models/budget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+part of 'services.dart';
 
 class BudgetService {
-  final Dio dio = Dio();
+  final BaseApi api = BaseApi();
 
-  Future<List<BudgetModel>> fetchBudgets() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('token') ?? '';
-    final response = await dio.get(
-      'https://www.catatuangku.site/api/budgets/',
-      options: Options(headers: {'Authorization': 'Bearer $token'}),
-    );
-    final data = response.data as List;
-    return data.map((e) => BudgetModel.fromJson(e)).toList();
+  Future<List<BudgetModel>?> getAll() async {
+    final Response response = await api.get('/budgets');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      final List<BudgetModel> budgets = data
+          .map((e) => BudgetModel(
+                id: e['_id'],
+                name: e['name'],
+                amount: e['amount'],
+                period: e['period'],
+                categories: (e['categories'] as List<dynamic>).map((e) => e.toString()).toList(),
+                walletIds: (e['walletIds'] as List<dynamic>).map((e) => e.toString()).toList(),
+                usedAmount: e['usedAmount'],
+                remainingAmount: e['remainingAmount'],
+                percentUsed: e['percentUsed'],
+                startDate: DateFormat('yyyy-MM-ddTHH:mm:ss.SSSZ').parse(e['startDate']),
+              ))
+          .toList();
+
+      return budgets;
+    }
+
+    return null;
+  }
+
+  Future<bool> addNew(BudgetModel budget) async {
+    final Response response = await api.post('/budgets', data: {
+      'name': budget.name,
+      'amount': budget.amount,
+      'categories': budget.categories,
+      'walletIds': budget.walletIds,
+      'period': budget.period,
+      'startDate': budget.startDate.toString(),
+    });
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+
+    throw Exception('Gagal menambahkan anggaran');
   }
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
+import 'package:fe_catat_uangku/models/budget.dart';
 import 'package:fe_catat_uangku/models/payment_planning.dart';
 import 'package:fe_catat_uangku/utils/base_api.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
 part 'payment_planning_service.dart';
+part 'budget_service.dart';
